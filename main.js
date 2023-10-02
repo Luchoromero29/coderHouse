a69461c3d5ed9589147bd4269036d49b510ac975
Luchoromero29 <luchonromero@gmail.com>
2023-10-02 10:29:02 -0300

    correccion de main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 // algoritmo para calcular el presupeusto de una computadora
 
-<<<<<<< HEAD
+
 
 
 let precioMonitor, precioTeclado, precioMouse, precioGabinete, precioTotal;
-=======
-let precioMonitor, precioTeclado, precioMouse, precioGabinete, precioTotal, descuento;
->>>>>>> 02ccb6905b39addd4d187a6770c8569bbe5ef6b7
+
 
 do {
   precioGabinete = Number(prompt("Introduzca el precio de su gabinete"));
@@ -27,7 +25,6 @@ do {
 precioTotal = precioGabinete + precioMonitor + precioMouse + precioTeclado;
 
 if (precioTotal > 150000) {
-<<<<<<< HEAD
   calculoDescuentos();
 }
 
@@ -39,18 +36,9 @@ alert(`Gracias por comprar con 'AWSD Tecnologia' `);
 function calculoDescuentos() {
 
   let descuento;
-=======
->>>>>>> 02ccb6905b39addd4d187a6770c8569bbe5ef6b7
+
   alert(`Debido al costo de la compra tendras un 10% de descuento`);
   descuento = precioTotal * 0.1;
   precioTotal = precioTotal - descuento;
   alert(`Tu descuento fue de ${descuento}`);
-<<<<<<< HEAD
-
 }
-=======
-}
-
-alert(`El costo total de su computadora es: ${precioTotal}`);
-alert(`Gracias por comprar con 'AWSD Tecnologia' `);
->>>>>>> 02ccb6905b39addd4d187a6770c8569bbe5ef6b7
